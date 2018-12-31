79e6404572de65b28950bfd2d0ef71f7e101fe7d
Sonatics77 <thenuga.dealwis@gmail.com>
2018-12-31 14:39:24 +1100

    choose fix6

--- commands/choose.js
+++ commands/choose.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (Bot, message, args) => {
   let cmdargs = message.content.slice(1).trim().split(" ");
   let argus = cmdargs.splice(0,1);
-  let chosen = argus[Math.floor(Math.random() * args.length)]
+  let chosen = cmdargs[Math.floor(Math.random() * args.length)]
   return message.channel.send(chosen);
 }
 
