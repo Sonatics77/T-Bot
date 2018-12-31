45a16315ecda9690ce217e5af0cd4747c9e59ea4
Sonatics77 <thenuga.dealwis@gmail.com>
2018-12-31 23:23:33 +1100

    time update2

--- commands/time.js
+++ commands/time.js
@@ -1,9 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
+  let csplittime = message.createdAt.split(" ");
+  let ctimelength = csplittime.length;
+  let ctime = csplittime.splice(0,(ctimelength-1));
   let embedTime = new Discord.RichEmbed()
   .setDescription("The current time is:")
-  .setDescription(message.createdAt);
+  .setDescription(ctime);
 
   return message.channel.send(embedTime);
 }
