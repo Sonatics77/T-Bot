7dac74955ada233c09e62b1ad1c332a77a62635e
Sonatics77 <thenuga.dealwis@gmail.com>
2018-12-31 23:57:54 +1100

    embed timer

--- commands/embed.js
+++ commands/embed.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
-  let embedText = message.content.slice(7);
-  let responseEmbed = new Discord.RichEmbed()
-  .setDescription(embedText)
-  .setColor("#42f4b3");
+  setTimeout(function(){
+    let embedText = message.content.slice(7);
+    let responseEmbed = new Discord.RichEmbed()
+    .setDescription(embedText)
+    .setColor("#42f4b3");
+  }, ms(60000));
+
 
   return message.channel.send(responseEmbed);
 
