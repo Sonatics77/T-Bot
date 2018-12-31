7a3596690c960cc0bbdf8df54b633bab602367d6
Sonatics77 <thenuga.dealwis@gmail.com>
2018-12-31 14:34:13 +1100

    choose fix3

--- commands/choose.js
+++ commands/choose.js
@@ -4,7 +4,7 @@ module.exports.run = async (Bot, message, args) => {
   let cmdargs = message.content.slice(Prefix.length).trim().split(" ");
   let argus = cmdargs.splice(0,1);
   let chosen = argus[Math.floor(Math.random() * args.length)]
-  return.message.channel.send(rand);
+  return message.channel.send(rand);
 }
 
 module.exports.help = {
