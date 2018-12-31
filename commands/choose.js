6b31a14e4909ce4a2d5568488dbdccc509919f11
Sonatics77 <thenuga.dealwis@gmail.com>
2018-12-31 14:47:21 +1100

    Update commands/choose.js

--- commands/choose.js
+++ commands/choose.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
-  let cmdargs1 = message.content.slice(1).trim().split(",");
+  let cmdargs1 = message.content.slice(1).trim().split(", ");
   let argus = cmdargs1.splice(0,1);
-  let cmdargs2 = cmdargs1.split(" or ");
   let chosen = cmdargs1[Math.floor(Math.random() * cmdargs1.length)]
   return message.channel.send(chosen);
 }
