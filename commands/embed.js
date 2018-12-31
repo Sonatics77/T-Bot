65e70029880fd4b5f168a18d6a8069c94501e8e1
Sonatics77 <thenuga.dealwis@gmail.com>
2018-12-31 20:40:22 +1100

    add embed command

---
+++ commands/embed.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (Bot, message, args) => {
+  let embedText = message.content.slice(1);
+  let responseEmbed = new Discord.RichEmbed()
+  .setDescription(embedText)
+
+  return message.channel.send(responseEmbed);
+
+}
+
+
+module.exports.help = {
+  name: "embed"
+}
