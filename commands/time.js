3fb1e41d6a586d1e4112965f8938466c88e584cf
Sonatics77 <thenuga.dealwis@gmail.com>
2018-12-31 23:25:51 +1100

    time fix5

--- commands/time.js
+++ commands/time.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
-  let csplittime = message.createdAt.split(" ");
+  let crawtime = message.createdAt;
+  let csplittime = crawtime.split(" ")
   let ctimelength = csplittime.length;
   let ctime = csplittime.splice(0,(ctimelength-1));
   let embedTime = new Discord.RichEmbed()
