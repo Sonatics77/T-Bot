6d6c987f3f99645584bd534cbe5f5e6124f03cab
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-01 20:54:07 +1100

    embed revert

--- commands/embed.js
+++ commands/embed.js
@@ -1,14 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
-  setTimeout(function(){
     let embedText = message.content.slice(7);
     let responseEmbed = new Discord.RichEmbed()
     .setDescription(embedText)
     .setColor("#42f4b3");
-  }, ms(60000));
-
-
+    
   return message.channel.send(responseEmbed);
 
 }
