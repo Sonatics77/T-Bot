a5f38230cece038c2fad2d0a55f31253553cb9d1
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-01 23:10:30 +1100

    embed join fix add pink

--- commands/embed.js
+++ commands/embed.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
-    let colourName = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white"];
-    let colourHex = ["#ff0000", "#ff6100", "#ffdd00", "#19ff00", "#0087ff", "#9400ff", "#000000", "#ffffff"];
-    let eText = message.content.slice(7).split(" ");
-    let emColour = `${eText.splice(0,2)}`;
+    let colourName = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "black", "white"];
+    let colourHex = ["#ff0000", "#ff6100", "#ffdd00", "#19ff00", "#0087ff", "#9400ff", "#fc00a8", "#000000", "#ffffff"];
+    let emText = message.content.slice(7).split(" ");
+    let emColour = `${emText.splice(0,2)}`;
     let eColour = `${emColour.slice(1)}`;
     let colourPos = colourName.indexOf(eColour);
+    let eText = emText.join(" ");
     var eHex = 0
     if (colourPos == -1) {
       eHex = eColour;
