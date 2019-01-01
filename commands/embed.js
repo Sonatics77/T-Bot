280e7c2814df3a08ff50f47512bdd0f931f03806
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-01 22:29:56 +1100

    embed test23

--- commands/embed.js
+++ commands/embed.js
@@ -3,17 +3,16 @@ const Discord = require("discord.js");
 module.exports.run = async (Bot, message, args) => {
     let colourName = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white"];
     let colourHex = ["#ff0000", "#ff6100", "#ffdd00", "#19ff00", "#0087ff", "#9400ff", "#000000", "#ffffff"];
-    let embedText = message.content.slice(7).split(" ");
-    let embedColour = embedText.splice(0,2);
-    let asfgs = embedColour.slice(2, embedColour.length - 1);
-    let colourPos = colourName.indexOf(asfgs);
-    let embedHex = colourHex[colourPos];
+    let eText = message.content.slice(7).split(" ");
+    let eColour = `"${eText.splice(0,2)}"`;
+    let colourPos = colourName.indexOf(eColour);
+    let eHex = colourHex[colourPos];
 
     let responseEmbed = new Discord.RichEmbed()
-    .setDescription(embedText)
-    .setColor(embedHex);
+    .setDescription(eText)
+    .setColor(eHex);
 
-    message.channel.send(embedColour);
+    message.channel.send(eColour);
     message.channel.send(colourPos);
     message.channel.send(asfgs);
     message.channel.send(embedText);
