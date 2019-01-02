57875c7331abfdc7fc71f168fb36eab65bf9129a
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-02 17:19:09 +1100

    Update commands/announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -3,10 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (Bot, message, args) => {
   let aArgs = message.content.slice(10).split(" ");
   if(! message.member.hasPermission("MENTION_EVERYONE")) return message.channel.send("You do not have the required permissions to use this command");
-  let aChannel = `${aArgs.splice(0,2)}`;
-  let anChannel = 0 // `${anChannel.slice(1)}`;
+  let anChannel = `${aArgs.splice(0,2)}`;
+  let annChannel = ["<",`${anChannel.slice(2)}`];
+  let aChannel = annChannel.join("")
   let aText = aArgs.join(" ");
-  let sendChannel = message.guild.channels.find(channel => channel.name == aChannel);
+  let sendChannel = message.guild.channels.find(channel => channel.id == aChannel);
 
   message.channel.send(aChannel);
   message.channel.send(aText);
