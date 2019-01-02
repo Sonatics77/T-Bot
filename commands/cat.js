c0c799cb48d754378ca5ceee77c9e3bc4274ce9e
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-02 20:23:13 +1100

    cat add

---
+++ commands/cat.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (Bot, message, args) => {
+  let {body} = await superagent
+  .get(`http://aws.random.cat/meow`);
+
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+  let catembed = new Discord.RichEmbed()
+  .setColour(randomColour)
+  .setTitle("Cat uwu");
+  .setImage(body.url);
+
+  message.channel.send(catembed);
+
+}
+
+module.exports.help = {
+  name: "cat"
+}
