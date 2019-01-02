5f052e84c1377ed8151f429ef4f38c9088692ee9
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-02 21:13:33 +1100

    Update commands/cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -3,10 +3,16 @@ const snekfetch = require("snekfetch");
 const api = "http://aws.random.cat/meow"
 
 module.exports.run = async (Bot, message, args) => {
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
   snekfetch.get(api).then(r => {
     let body = r.body
 
-    message.channel.send(body);
+    let catembed = new Discord.RichEmbed()
+    .setColor(randomColor)
+    .setTitle("Cat uwu")
+    .setImage(body);
+
+    message.channel.send(catembed);
   })
 }
 
