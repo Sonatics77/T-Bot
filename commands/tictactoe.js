060c41b051dfed67403c5916c22fc4fcbd10a617
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-02 17:49:18 +1100

    Update commands/tictactoe.js

---
+++ commands/tictactoe.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (Bot, message, args) => {
+
+  message.channel.send("t
+  e
+  s
+  t");
+}
+
+module.exports.help = {
+  name: "ttt"
+}
