4f359b2911452804def3fe798c6511e0d8e0fd4b
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-02 20:38:07 +1100

    Update commands/cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const Superagent = require("superagent");
 
 module.exports.run = async (Bot, message, args) => {
-  let {body} = await superagent
+  let {body} = await Superagent
   .get(`http://aws.random.cat/meow`);
 
   var randomColour = Math.floor(Math.random()*16777215).toString(16);
