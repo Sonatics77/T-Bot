db677b41f01e63dc10bdcf0a0ed15501213e3f04
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-02 21:50:46 +1100

    vcjoin add

---
+++ commands/vcjoin.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (Bot, message, args) => {
+  voiceChannel.join().then(connection => {
+
+  })
+}
+
+module.exports.help = {
+  name: "join"
+}
