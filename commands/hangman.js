8d37e198af010ce6bc8abd8e4aaaa2f88bdce23b
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-05 16:08:04 +1100

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -3,11 +3,19 @@ var randomWord = require('random-words');
 
 module.exports.run = async (Bot, message, args) => {
   let hword = (randomWord({min: 7, max: 15}));
+  message.channel.send(hword);
+  var i;
+  var hsword = [];
+  for (i = 0; i < hword.length+1; i++) {
+    hsword.push("_");
+  }
   const msgs = await message.channel.awaitMessages(msg => msg.content.includes("t.hm"), {maxMatches: 1});
   if(! msgs){
     return message.channel.send("Send a valid response");
   } else {
     message.channel.send(`${msgs.map(msg => msg.content)}`);
+    var hletter = msg.content.slice(5).trim();
+    if (hword.includes(hletter))
   }
 
 }
