370ea1c2c00b31ef1670c72d8175e65ca817491b
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-05 15:19:22 +1100

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -2,7 +2,9 @@ const Discord = require("discord.js");
 var randomWord = require('random-words');
 
 module.exports.run = async (Bot, message, args) => {
-  message.channel.send(randomWord());
+  let hword = (randomWord({min: 7, max: 15}));
+  var rCollection = new Discord.MessageCollector(message.channel, msg => msg.content.includes("t.hm"), {maxMatches: 1});
+  message.channel.send(rCollection);
 }
 
 module.exports.help = {
