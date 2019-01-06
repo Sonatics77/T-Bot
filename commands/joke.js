97593b51d1e7dcc6688bdeb50dd973cb9100eb99
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-06 20:47:02 +1100

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -4,7 +4,7 @@ const api = "https://icanhazdadjoke.com/"
 
 module.exports.run = async (Bot, message, args) => {
   snekfetch.get(api).then(r => {
-    let jokebody = r.body
+    let {body} = r.body
 
     message.channel.send(jokebody);
   })
