7ad1720fb7d9ee6f96f292167985e5ece5392cca
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-06 13:13:40 +1100

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -33,9 +33,9 @@ module.exports.run = async (Bot, message, args) => {
           hsword[indices[i3]] = hletter;
         }
         let sendArr = [`Unknown: ${hsword.join(" ")}`,`Used: ${uletters.join("")}`];
-        let lmsg = message.channel.lastMessageID;
-        message.channel.send(lmsg);
         message.channel.send(sendArr)//.then(d_msg => d_msg.delete(10000));
+        var lmsg = message.channel.lastMessageID;
+        message.channel.send(lmsg);
         //lmsg.delete();
       }
     }
