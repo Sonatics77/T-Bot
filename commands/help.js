01b6ce34b3c70b1060b923f3a526b7cdcd7cde39
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-08 21:44:42 +1100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,11 +8,11 @@ module.exports.run = async (Bot, message, args) => {
   .setThumbnail(boticon)
   .setColor("#137ab2")
   .setDescription("Prefix is t\.")
-  .setField("Moderation", "t.report @user reason, t.ban @user, t.kick @user, t.tempmute @user 1m, t.unmute @user, t.addrole role, t.removerole role")
-  .setField("Info", "t.serverinfo, t.botinfo")
-  .setField("Games", "t.hangman")
-  .setField("Announcements", "t.announce #channel \"text\"")
-  .setField("Other", "t.cat, t.embed \"colour (or hex code)\", t.choose option1 option 2... etc");
+  .addField("Moderation", "t.report @user reason, t.ban @user, t.kick @user, t.tempmute @user 1m, t.unmute @user, t.addrole role, t.removerole role")
+  .addField("Info", "t.serverinfo, t.botinfo")
+  .addField("Games", "t.hangman")
+  .addField("Announcements", "t.announce #channel \"text\"")
+  .addField("Other", "t.cat, t.embed \"colour (or hex code)\", t.choose option1 option 2... etc");
 
   message.channel.send(eHelp);
 
