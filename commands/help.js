aa933e2d3ce0f6409774882f83626430d2022b30
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-08 21:38:03 +1100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
-  var eHelp = new Discord.RichEmbed()
+
+  let boticon = bot.user.displayAvatarURL
+  let eHelp = new Discord.RichEmbed()
   .setTitle("TaskieBot Commands")
+  .setThumbnail(boticon)
+  .setColor("#137ab2")
   .setDescription("Prefix is t\.")
   .setField("Moderation", "t.report @user reason, t.ban @user, t.kick @user, t.tempmute @user 1m, t.unmute @user, t.addrole role, t.removerole role")
   .setField("Info", "t.serverinfo, t.botinfo")
