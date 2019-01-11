04ee5a518b651ca844eca642f87571d982cac32d
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-11 15:11:50 +1100

    Update tictactoe.js

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -3,8 +3,13 @@ var rn = require('random-number');
 
 
 module.exports.run = async (Bot, message, args) => {
-
-  var tBoard = [":three: :white_large_square: :white_large_square: :white_large_square:", ":two: :white_large_square: :white_large_square: :white_large_square:", ":one: :white_large_square: :white_large_square: :white_large_square:", ":black_large_square: :regional_indicator_a: :regional_indicator_b: :regional_indicator_c:"];
+  var row3 = [":three:",":white_large_square:",":white_large_square:",":white_large_square:"];
+  var row2 = [":two:",":white_large_square:",":white_large_square:",":white_large_square:"];
+  var row1 = [":one:",":white_large_square:",":white_large_square:",":white_large_square:"];
+  var bRow3 = row3.join(" ");
+  var bRow2 = row2.join(" ");
+  var bRow1 = row1.join(" ");
+  var tBoard = [bRow3,bRow2,bRow1,":black_large_square: :regional_indicator_a: :regional_indicator_b: :regional_indicator_c:"];
   var options = {
     min:  0
   , max:  1
@@ -21,6 +26,7 @@ module.exports.run = async (Bot, message, args) => {
   console.log(userB);
   var tChannel= message.guild.channels.find(channel => channel.id == message.channel.id);
   tChannel.send(tBoard);
+  var boardID = tChannel.lastMessageID(); //boardID
   await tChannel.send(`Waiting for ${tArgs[1]} to reply with \"yes\"...`);
   const msgs = await tChannel.awaitMessages(msg => msg.author.id == userB, {maxMatches: 1});
   tChannel.send("Starting tic tac toe game...");
@@ -31,7 +37,9 @@ module.exports.run = async (Bot, message, args) => {
     xORo = userB
     await tChannel.send(`${tArgs[1]} is starting as :o: !`);
   }
-
+  tChannel.fetchMessage(boardID).then(async emsg => {
+    await emsg.edit("test");
+  })
   var fin = 0;
   while(fin == 0){
 
