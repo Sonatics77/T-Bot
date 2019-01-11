c567ea6bda468d1ba699388085c2fefc6265a8df
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-11 13:32:15 +1100

    Update tictactoe.js

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
 
+  let tUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   let tArgs = message.content.trim().split(" ");
   let userA = message.author.id;
   let user2B = tArgs[1]
@@ -9,7 +10,7 @@ module.exports.run = async (Bot, message, args) => {
   console.log(userA);
   console.log(userB);
   let tChannel= message.guild.channels.find(channel => channel.id == message.channel.id);
-  await tChannel.send(`Waiting for ${tArgs[1]}`);
+  await tChannel.send(`Waiting for ${tArgs[1]}...`);
   const msgs = await tChannel.awaitMessages(msg => msg.author.id == userB, {maxMatches: 1});
   await tChannel.send("works");
 
