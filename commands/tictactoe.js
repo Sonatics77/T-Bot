d10bfafe39f0404bcfcd5ea5333af548d7dad0cd
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-11 13:14:22 +1100

    Update tictactoe.js

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (Bot, message, args) => {
 
   let tArgs = message.content.trim().split(" ");
-  let userA = message.author;
-  let userB = tArgs[1];
+  let userA = message.author.id;
+  let userB = tArgs[1].slice(4,tArgs[1].length-1);
   console.log(userA);
   console.log(userB);
   console.log(tArgs);
