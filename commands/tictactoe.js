d604dabf4aa2e7f1f3f66aadf913ad937aa62068
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-11 13:46:15 +1100

    Update tictactoe.js

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -2,23 +2,33 @@ const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
 
-  let tUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  var tBoard = ["    |    |", "__|__|__", "    |    |", "__|__|__", "    |    |"];
+
+  var tUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(! tUser) return message.channel.send("Can't find user!");
-  let tArgs = message.content.trim().split(" ");
-  let userA = message.author.id;
-  let user2B = tArgs[1]
-  let userB = user2B.slice(3,tArgs[1].length-1);
+  var tArgs = message.content.trim().split(" ");
+  var userA = message.author.id;
+  var user2B = tArgs[1]
+  var userB = user2B.slice(3,tArgs[1].length-1);
   console.log(userA);
   console.log(userB);
-  let tChannel= message.guild.channels.find(channel => channel.id == message.channel.id);
+  var tChannel= message.guild.channels.find(channel => channel.id == message.channel.id);
+  tChannel.send(tBoard);
   await tChannel.send(`Waiting for ${tArgs[1]} to reply with \"yes\"...`);
   const msgs = await tChannel.awaitMessages(msg => msg.author.id == userB, {maxMatches: 1});
-  await tChannel.send("Starting tic tac toe game...");
-
-
+  tChannel.send("Starting tic tac toe game...");
+  var fin? = 0;
+  while(fin == 0){
+    const msgs = await tChannel.awaitMessages(msg => msg.author.id == userB, {maxMatches: 1});
+  }
   }
 
 
   module.exports.help = {
     name: "ttt"
   }
+    |    |
+__|__|__
+    |    |
+__|__|__
+    |    |
