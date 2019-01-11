9981bbe878312c5bdb5e011da979cf2fede1d63e
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-11 13:35:38 +1100

    thing

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (Bot, message, args) => {
 
   let tUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(! bUser) return message.channel.send("Can't find user!");
   let tArgs = message.content.trim().split(" ");
   let userA = message.author.id;
   let user2B = tArgs[1]
@@ -10,9 +11,9 @@ module.exports.run = async (Bot, message, args) => {
   console.log(userA);
   console.log(userB);
   let tChannel= message.guild.channels.find(channel => channel.id == message.channel.id);
-  await tChannel.send(`Waiting for ${tArgs[1]}...`);
+  await tChannel.send(`Waiting for ${tArgs[1]} to reply with \"yes\"...`);
   const msgs = await tChannel.awaitMessages(msg => msg.author.id == userB, {maxMatches: 1});
-  await tChannel.send("works");
+  await tChannel.send("Starting tic tac toe game...");
 
 
   }
