3cccee2b87ad53542cffce4784ede47ab5cb0a3f
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-16 15:54:28 +1100

    Create join_channel.js

---
+++ commands/join_channel.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (Bot, message, args) => {
+  console.log(message.member.voiceChannel);
+  if(message.member.voiceChannel){
+    console.log("1");
+    if(message.guild.voiceConnection){
+      console.log("2");
+      message.member.voiceChannel.join();
+    }
+  }
+}
+  module.exports.help = {
+    name: "8ball"
+  }
