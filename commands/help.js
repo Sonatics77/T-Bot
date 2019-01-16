751201bc564fcf4806a042126b07828c106b5bb9
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-16 20:23:36 +1100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (Bot, message, args) => {
+  let prefix = prefixes[message.guild.id].prefixes;
 
   let boticon = Bot.user.displayAvatarURL
   let eHelp = new Discord.RichEmbed()
