aea7f651bf2c0855fb2d251dda5fb3a384f70333
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-16 20:10:32 +1100

    help update prefix add

--- commands/help.js
+++ commands/help.js
@@ -7,12 +7,12 @@ module.exports.run = async (Bot, message, args) => {
   .setTitle("TaskieBot Commands")
   .setThumbnail(boticon)
   .setColor("#137ab2")
-  .setDescription("Prefix is t\.")
-  .addField("Moderation", "t.report @user reason, t.ban @user, t.kick @user, t.tempmute @user 1m, t.unmute @user, t.addrole role, t.removerole role")
+  .setDescription(`Prefix is ${prefix}. Can be changed with t.prefix <prefix>`)
+  .addField("Moderation", "t.report @user reason, t.ban @user, t.kick @user, t.tempmute @user 1m, t.unmute @user, t.addrole <role>, t.removerole <role>")
   .addField("Info", "t.serverinfo, t.botinfo")
   .addField("Games", "t.hangman")
-  .addField("Announcements", "t.announce #channel \"text\"")
-  .addField("Other", "t.cat, t.embed \"colour (or hex code)\" \"text\", t.choose option1 option 2... etc");
+  .addField("Announcements", "t.announce #channel <text>")
+  .addField("Other", "t.cat, t.embed <colour (or hex code)> <text>, t.choose <option1>, <option 2>,... etc");
 
   message.channel.send(eHelp);
 
--- commands/prefix.js
+++ commands/prefix.js
@@ -3,9 +3,22 @@ const Fs = require("fs");
 
 module.exports.run = async (Bot, message, args) => {
 
-  }
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You do not have the permissions to use this command");
+  if(!args[0]) return message.channel.send("Provide a new prefix")
+
+  let prefixes = JSON.parse(Fs.readFileSync("./prefixes.json", "utf8"));
+
+  prefixes[message.guild.id] = {
+    prefixes: args[0]
+  };
+
+  Fs.writeFile("./prefixes.json", JSON.stringify(prefixes),);
+
+  return message.channel.send(`The new prefix for TaskieBot has been set to: ${args[0]}`);
+  
+}
 
 
-  module.exports.help = {
-    name: "prefix"
-  }
+module.exports.help = {
+  name: "prefix"
+}
