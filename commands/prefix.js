57c85c525f6929cc9681dbd7f8ae1d5ec25b8a4b
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-16 20:20:58 +1100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -5,19 +5,19 @@ module.exports.run = async (Bot, message, args) => {
   var pArgs = message.content.split(" ");
 
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You do not have the permissions to use this command");
-  if(!pArgs[0]) return message.channel.send("Provide a new prefix")
+  if(!pArgs[1]) return message.channel.send("Provide a new prefix")
 
   let prefixes = JSON.parse(Fs.readFileSync("./prefixes.json", "utf8"));
 
   prefixes[message.guild.id] = {
-    prefixes: pArgs[0]
+    prefixes: pArgs[1]
   };
 
   Fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
     if (err) console.log(err)
   });
 
-  return message.channel.send(`The new prefix for TaskieBot has been set to: ${pArgs[0]}`);
+  return message.channel.send(`The new prefix for TaskieBot has been set to: ${pArgs[1]}`);
 
 }
 
