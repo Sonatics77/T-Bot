b7d2230ce2955077a4bc7bc9e21579b0fbc53b22
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-16 20:14:56 +1100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -2,20 +2,20 @@ const Discord = require("discord.js");
 const Fs = require("fs");
 
 module.exports.run = async (Bot, message, args) => {
-  console.log(args);
+  var pArgs = message.split(" ");
 
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You do not have the permissions to use this command");
-  if(!args[0]) return message.channel.send("Provide a new prefix")
+  if(!pArgs[0]) return message.channel.send("Provide a new prefix")
 
   let prefixes = JSON.parse(Fs.readFileSync("./prefixes.json", "utf8"));
 
   prefixes[message.guild.id] = {
-    prefixes: args[0]
+    prefixes: pArgs[0]
   };
 
   Fs.writeFile("./prefixes.json", JSON.stringify(prefixes),);
 
-  return message.channel.send(`The new prefix for TaskieBot has been set to: ${args[0]}`);
+  return message.channel.send(`The new prefix for TaskieBot has been set to: ${pArgs[0]}`);
 
 }
 
