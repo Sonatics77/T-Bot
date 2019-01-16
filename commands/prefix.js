7832f37491571d3b7f1c2a41a00a3b8e2359922a
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-16 20:16:03 +1100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const Fs = require("fs");
 
 module.exports.run = async (Bot, message, args) => {
-  var pArgs = message.split(" ");
+  var pArgs = message.content.split(" ");
 
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You do not have the permissions to use this command");
   if(!pArgs[0]) return message.channel.send("Provide a new prefix")
