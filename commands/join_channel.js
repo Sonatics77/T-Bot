d5b2ad54f611af648527e59d1459810085e2da1b
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-16 17:27:27 +1100

    Update join_channel.js

--- commands/join_channel.js
+++ commands/join_channel.js
@@ -3,12 +3,13 @@ var ffmpeg = require('FFMPEG WORK');
 
 module.exports.run = async (Bot, message, args) => {
   console.log(message.member.voiceChannel);
+  var vChannel = message.guild.channels.find(channel => channel.id == message.member.voiceChannel);
   if(message.member.voiceChannel){
     console.log("1");
     if(! message.guild.voiceConnection){
       console.log("2");
 
-      message.member.voiceChannel.join();
+      vChannel.join();
     }
   }
 }
