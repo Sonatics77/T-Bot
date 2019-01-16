2bfd383dd45aed80f48c466c92a0971335a5a28a
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-16 19:56:48 +1100

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ Bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  let prefixes = JSON.parse(Fs.readFileSync("./prefixes.json", "utf8"));
 
   if(!prefixes[message.guild.id]) {
     prefixes[message.guild.id] = {
