f820552881d760f53989fffc21d5874c89d800eb
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-16 20:32:44 +1100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,15 +4,15 @@ module.exports.run = async (Bot, message, args, prefix) => {
 
   let boticon = Bot.user.displayAvatarURL
   let eHelp = new Discord.RichEmbed()
-  .setTitle("TaskieBot Commands")
+  .setTitle(`TaskieBot Commands`)
   .setThumbnail(boticon)
-  .setColor("#137ab2")
+  .setColor(`#137ab2`)
   .setDescription(`Prefix is ${prefix} Can be changed with ${prefix}prefix <prefix>`)
-  .addField("Moderation", "t.report @user reason, t.ban @user, t.kick @user, t.tempmute @user 1m, t.unmute @user, t.addrole <role>, t.removerole <role>")
-  .addField("Info", "t.serverinfo, t.botinfo")
-  .addField("Games", "t.hangman")
-  .addField("Announcements", "t.announce #channel <text>")
-  .addField("Other", "t.cat, t.embed <colour (or hex code)> <text>, t.choose <option1>, <option 2>,... etc");
+  .addField(`Moderation`, `${prefix}report @user reason, ${prefix}ban @user, ${prefix}kick @user, ${prefix}tempmute @user 1m, ${prefix}unmute @user, ${prefix}addrole <role>, ${prefix}removerole <role>`)
+  .addField(`Info`, `${prefix}serverinfo, ${prefix}botinfo`)
+  .addField(`Games`, `${prefix}hangman`)
+  .addField(`Announcements`, `${prefix}announce #channel <text>`)
+  .addField(`Other`, `${prefix}cat, ${prefix}embed <colour (or hex code)> <text>, ${prefix}choose <option1>, <option 2>,... etc`);
 
   message.channel.send(eHelp);
 
