e8d7595d9e609d592e715a8e55afd77d0a189912
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-21 10:29:38 +1100

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const Ffmpeg = require("ffmpeg");
 
 module.exports.run = async (Bot, message, args, prefix) => {
-  let jChannel = await message.member.voiceChannel;
+  let jrChannel = await message.member.voiceChannelID;
+  let jChannel = await message.guild.channels.find(channel => channel.id == jrChannel);
   console.log(jChannel);
   jChannel.join().then(connection => {
     console.log("CONNECTED");
