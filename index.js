ba4d57b5b12d15c01c4393d5d0d6047caab95c45
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-21 09:35:48 +1100

    log try

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ Fs.readdir("./commands/", (err, files) => {
 Bot.on("ready", async () => {
   console.log(`${Bot.user.username} is online!`)
   Bot.user.setActivity("Jungkook ♥ ♥ ♥", {type: "LISTENING"});
+  let logFile = Bot.commands.get("log");
+  console.log(logFile);
+  if(logFile) logFile.run();
 });
 
 Bot.on("message", async message => {
