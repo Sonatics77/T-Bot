7adb8f7e86d3da44119dd204957273c47636a216
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-22 23:10:36 +1100

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const Ffmpeg = require("C:\\Users\\thenu\\Documents\\TaskieBot\\T-Bot\\node_modules\\ffmpeg");
+const Ffmpeg = require("\\node_modules\\ffmpeg");
 
 module.exports.run = async (Bot, message, args, prefix) => {
   var vChannel = message.member.voiceChannel;
