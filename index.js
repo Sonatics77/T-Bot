14858fb0f25d9e44c811fbd009ea9780c1dc34e4
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-22 23:30:18 +1100

    Update index.js

--- index.js
+++ index.js
@@ -89,6 +89,40 @@ Bot.on("message", async message => {
 }
 });
 
+Bot.on("ready", async => {
+  while(0==0){
+  setTimeout(Jungkook,10000);
+  setTimeout(Jimin,10000);
+  setTimeout(V,10000);
+  setTimeout(Suga,10000);
+  setTimeout(RM,10000);
+  setTimeout(Jin,10000);
+  setTimeout(JHope,10000);
+}
+
+  function Jungkook () {
+    Bot.user.setActivity("Jungkook ♥ ♥ ♥ ♥ ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function Jimin () {
+    Bot.user.setActivity("Jimin ♥ ♥ ♥ ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function V () {
+    Bot.user.setActivity("V ♥ ♥ ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function Suga () {
+    Bot.user.setActivity("Suga ♥ ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function RM () {
+    Bot.user.setActivity("RM ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function Jin () {
+    Bot.user.setActivity("Jin ♥ ♥", {type: "LISTENING"});
+  }
+  function JHope () {
+    Bot.user.setActivity("J-Hope ♥", {type: "LISTENING"});
+  }
+});
+
 
 
 
