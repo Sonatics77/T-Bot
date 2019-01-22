67f1ed2e39ee3733365fd4d1cddc0b87cba7afa3
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-22 23:47:22 +1100

    cycle

---
+++ commands/cycle.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (Bot, message, args, prefix) => {
+  while(){
+  setTimeout(Jungkook,10000);
+  setTimeout(Jimin,10000);
+  setTimeout(V,10000);
+  setTimeout(Suga,10000);
+  setTimeout(RM,10000);
+  setTimeout(Jin,10000);
+  setTimeout(JHope,10000);
+}
+
+  function Jungkook () {
+    Bot.user.setActivity("Jungkook ♥ ♥ ♥ ♥ ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function Jimin () {
+    Bot.user.setActivity("Jimin ♥ ♥ ♥ ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function V () {
+    Bot.user.setActivity("V ♥ ♥ ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function Suga () {
+    Bot.user.setActivity("Suga ♥ ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function RM () {
+    Bot.user.setActivity("RM ♥ ♥ ♥", {type: "LISTENING"});
+  }
+  function Jin () {
+    Bot.user.setActivity("Jin ♥ ♥", {type: "LISTENING"});
+  }
+  function JHope () {
+    Bot.user.setActivity("J-Hope ♥", {type: "LISTENING"});
+  }
+  module.exports.help = {
+    name: "kick"
+  }
