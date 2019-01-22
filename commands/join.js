d426ce3c235390e9733c920dd6f39fdbf078e804
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-22 22:27:12 +1100

    Create join.js

---
+++ commands/join.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const ffmpeg = require("ffmpeg");
+
+module.exports.run = async (Bot, message, args, prefix) => {
+  var vChannel = message.member.voiceChannel;
+  console.log(vChannel);
+  vChannel.join();
+}
+
+}
+  module.exports.help = {
+    name: "join"
+  }
