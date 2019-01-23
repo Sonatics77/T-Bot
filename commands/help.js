a690f0b7a4141e416ab96dcffee1d84e3b2c18a3
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-23 15:38:28 +1100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,9 +10,9 @@ module.exports.run = async (Bot, message, args, prefix) => {
   .setDescription(`Prefix is ${prefix} Can be changed with ${prefix}prefix <prefix>`)
   .addField(`Moderation`, `${prefix}report @user reason, ${prefix}ban @user, ${prefix}kick @user, ${prefix}tempmute @user 1m, ${prefix}unmute @user, ${prefix}addrole <role>, ${prefix}removerole <role>`)
   .addField(`Info`, `${prefix}serverinfo, ${prefix}botinfo`)
-  .addField(`Games`, `${prefix}hangman, ${prefix}ttt <@user>`)
+  .addField(`Games`, `${prefix}hangman, ${prefix}ttt <@user> (tic tac toe)`)
   .addField(`Announcements`, `${prefix}announce #channel <text>`)
-  .addField(`Other`, `${prefix}cat, ${prefix}embed <colour (or hex code)> <text>, ${prefix}choose <option1>, <option 2>,... etc`);
+  .addField(`Other`, `${prefix}cat, ${prefix}dadjoke, ${prefix}embed <colour (or hex code)> <text>, ${prefix}choose <option1>, <option 2>,... etc`);
 
   message.channel.send(eHelp);
 
