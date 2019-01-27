bfec3fdf62e12ec7cddb0af7a0fdee65cece8b2e
Sonatics77 <thenuga.dealwis@gmail.com>
2019-01-27 22:57:06 +1100

    reinstall node.js

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const FFMPEG = require("FFMPEG");
+const FFMPEG = require("ffmpeg");
 
 module.exports.run = async (Bot, message, args, prefix) => {
   var vChannel = message.member.voiceChannel;
