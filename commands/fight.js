3231e69764b5a6d17e94fc690cc7c0af19b30002
Sonatics77 <thenuga.dealwis@gmail.com>
2019-02-16 17:50:58 +1100

    Update fight.js

--- commands/fight.js
+++ commands/fight.js
@@ -4,19 +4,19 @@ module.exports.run = async (Bot,message,args,prefix) => {
   //var message.channel = message.guild.channels.find(channel => channel.id == message.channel.id);
   var fUser = message.mentions.users.first().id// || message.guild.members.get(args[0]));
   if(! fUser) return message.channel.send("Can't find that user");
-  var fTurnRand = Math.floor(Math.random() * 1);
+  //var fTurnRand = Math.floor(Math.random() * 1);
   //var p1Icon = message.author.displayAvatarURL
   //var p2Icon = fUser.displayAvatarURL
-  var p1Stat = ["100", "0"];
-  var p1DiStat = [`Health: ${p1Stat[1]}`, `Shield: ${p1Stat[2]}`];
-  var p2Stat = ["100", "0"];
-  var p2DiStat = [`Health: ${p2Stat[1]}`, `Shield: ${p2Stat[2]}`];
+  //var p1Stat = ["100", "0"];
+//  var p1DiStat = [`Health: ${p1Stat[1]}`, `Shield: ${p1Stat[2]}`];
+//  var p2Stat = ["100", "0"];
+//  var p2DiStat = [`Health: ${p2Stat[1]}`, `Shield: ${p2Stat[2]}`];
   //stats board
   console.log("works");
   console.log(fUser);
   console.log(message);
    //console.log(message.channel);
-  //message.channel.send("test");
+  message.channel.send("test");
 /*  var p1Board = new Discord.RichEmbed() //player1 embed
   .setTitle(`${message.author} vs ${fUser}`)
   .setColor(`#ef452b`)
